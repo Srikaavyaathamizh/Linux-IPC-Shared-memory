13c3a7525af343cee4e533ae111df16df1cbf613
Parrot User <user@localhost.localdomain>
2025-04-30 18:07:53 +0200

    catfile

--- sem.c
+++ sem.c
@@ -1,28 +1,3 @@
-# Linux-IPC-Shared-memory
-Ex06-Linux IPC-Shared-memory
-
-# AIM:
-To Write a C program that illustrates two processes communicating using shared memory.
-
-# DESIGN STEPS:
-
-### Step 1:
-
-Navigate to any Linux environment installed on the system or installed inside a virtual environment like virtual box/vmware or online linux JSLinux (https://bellard.org/jslinux/vm.html?url=alpine-x86.cfg&mem=192) or docker.
-
-### Step 2:
-
-Write the C Program using Linux Process API - Shared Memory
-
-### Step 3:
-
-Execute the C Program for the desired output. 
-
-# PROGRAM:
-
-## Write a C program that illustrates two processes communicating using shared memory.
-
-```
 //sem.c
 #include <stdio.h>
 #include <stdlib.h>
@@ -46,7 +21,7 @@ int main() {
 
     // Create shared memory
     shmid = shmget((key_t)1234, sizeof(struct shared_use_st), 0666 | IPC_CREAT);
-     if (shmid == -1) {
+ if (shmid == -1) {
         fprintf(stderr, "shmget failed\n");
         exit(EXIT_FAILURE);
     }
@@ -95,7 +70,7 @@ int main() {
         exit(EXIT_SUCCESS);
     } else {  // Parent process (Producer)
         char buffer[TEXT_SZ];
-  while (1) {
+while (1) {
             printf("Enter Some Text: ");
             fgets(buffer, TEXT_SZ, stdin);
 
@@ -120,7 +95,6 @@ int main() {
             fprintf(stderr, "shmdt failed\n");
             exit(EXIT_FAILURE);
         }
-        
         if (shmctl(shmid, IPC_RMID, 0) == -1) {
             fprintf(stderr, "shmctl failed\n");
             exit(EXIT_FAILURE);
@@ -130,14 +104,3 @@ int main() {
     }
 }
 
-
-```
-
-
-
-## OUTPUT
-
-![catfile](./im/Screenshot%20at%202025-04-30%2018-02-58.png)
-![catfile](./im/1.png)
-# RESULT:
-The program is executed successfully.
